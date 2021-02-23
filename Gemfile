90c6b5c7bc95f0a38841b2abb51c9692ddc6a402
minty1202 <aki.men1202@gmail.com>
2021-02-23 15:18:41 +0900

    日本語化

--- Gemfile
+++ Gemfile
@@ -55,3 +55,9 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+
+gem 'rails-i18n'
+
+gem 'devise-i18n'
+
+gem 'devise-i18n-views'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :nickname, uniqueness: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :nickname, presence: true, uniqueness: true,
+                        length: { minimum: 3, maximum: 15 }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+  # format: { with:  }
+  # format: { with: /\A[ァ-ヶー－]+\z/ }
+  # format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  # /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+  # /\A[ぁ-んァ-ン一-龥]/
+  # /\A[ァ-ヶー－]+\z/
 
 end
