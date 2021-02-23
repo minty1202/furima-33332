b1020f86387e25cb18c92b4499fb5f744c235788
minty1202 <aki.men1202@gmail.com>
2021-02-23 20:24:18 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, uniqueness: true,
+
+  with_options presence: true do
+    validates :nickname, uniqueness: true,
                        length: { minimum: 2, maximum: 6 }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_day, presence: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_day
+  end
   validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
 end
