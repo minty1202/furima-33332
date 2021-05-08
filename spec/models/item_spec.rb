00ea8c8764f3f5e8d31c2f9b32de0cd91862ce43
minty1202 <aki.men1202@gmail.com>
2021-05-08 19:07:19 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,10 +12,6 @@ RSpec.describe Item, type: :model do
       it 'name, text, price, category_id, status_id, fee_status_id, prefecture_id, delivery_idがあれば出品できる' do
         expect(item).to be_valid
       end
-      it 'priceが300..9999999の間であれば登録できる' do
-        item.price = 300
-        expect(item).to be_valid
-      end
     end
 
     context '新規出品できないとき' do
@@ -39,6 +35,25 @@ RSpec.describe Item, type: :model do
         item.valid?
         expect(item.errors.full_messages).to include("#{Item.human_attribute_name(:price)}を入力してください")
       end
+
+
+      it 'priceが半角英数混合では登録できない登録できない' do
+        item.price = '300a'
+        item.valid?
+        expect(item.errors.full_messages).to include("#{Item.human_attribute_name(:price)}は半角数値で入力してください")
+      end
+      it 'priceが半角英字のみでは登録できない登録できない' do
+        item.price = 'aaa'
+        item.valid?
+        expect(item.errors.full_messages).to include("#{Item.human_attribute_name(:price)}は半角数値で入力してください")
+      end
+      it 'priceが全角数字では登録できない登録できない' do
+        item.price = '３００'
+        item.valid?
+        expect(item.errors.full_messages).to include("#{Item.human_attribute_name(:price)}は半角数値で入力してください")
+      end
+
+
       it 'priceが300未満だと登録できない' do
         item.price = 299
         item.valid?
