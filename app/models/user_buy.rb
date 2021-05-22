1edd1efa5125dc1b691c8a577a9b8ea0e7039a4a
minty1202 <aki.men1202@gmail.com>
2021-05-22 21:55:51 +0900

    修正

--- app/models/user_buy.rb
+++ app/models/user_buy.rb
@@ -11,7 +11,6 @@ class UserBuy
     validates :phone_number, numericality: { only_integer: true }, length: { maximum: 11 }
     validates :token, presence: true
     validates :item_id
-    validates :order_id
   end
 
   def save
